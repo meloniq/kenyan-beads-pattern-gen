757148f451378cf6c259c321edf2f89eed8d763d
meloniq <meloniq@gmail.com>
2024-02-06 13:38:31 +0300

    fix generator tests after adding a required param

--- tests/test-generator.php
+++ tests/test-generator.php
@@ -4,28 +4,34 @@ use WP_Mock\Tools\TestCase;
 
 final class GeneratorTestCase extends TestCase {
 
+	public function setUp() : void {
+		$this->image_path = dirname( dirname( __FILE__ ) ) . '/assets/flag-poland.png';
+
+		parent::setUp();
+	}
+
 	public function test_empty() : void {
-		$generator = new Generator();
+		$generator = new Generator( $this->image_path );
 
 		$this->assertStringContainsString( 'bead-empty', $generator->empty() );
 	}
 
 	public function test_horizontal() : void {
-		$generator = new Generator();
+		$generator = new Generator( $this->image_path );
 
 		$this->assertStringContainsString( 'bead-horizontal', $generator->horizontal() );
 		$this->assertStringContainsString( 'bead-horizontal', $generator->horizontal( '#000000' ) );
 	}
 
 	public function test_vertical() : void {
-		$generator = new Generator();
+		$generator = new Generator( $this->image_path );
 
 		$this->assertStringContainsString( 'bead-vertical', $generator->vertical() );
 		$this->assertStringContainsString( 'bead-vertical', $generator->vertical( '#000000' ) );
 	}
 
 	public function test_generate_example() : void {
-		$generator = new Generator();
+		$generator = new Generator( $this->image_path );
 
 		$example = $generator->generate_example();
 
