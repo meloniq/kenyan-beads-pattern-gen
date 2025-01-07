15a12cffaa9cdd02dd5325f2703c9c33d66cc39e
Andrzej Piotrowski <apiotrowski@whitelabelcoders.com>
2025-01-07 12:58:08 +0100

    add vendor dir

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'meloniq/kenyan-beads',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '118877ff702d949af2186346a189ca7c49f45f9b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'meloniq/kenyan-beads' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '118877ff702d949af2186346a189ca7c49f45f9b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
